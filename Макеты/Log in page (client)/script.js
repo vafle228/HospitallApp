c4bb8806022dac05b9e0dda8e89711f28cade206
IOMOIO-hub <69336443+IOMOIO-hub@users.noreply.github.com>
2020-12-10 11:42:21 +0500

    Log in page (client) update
    uwu uwu uwu

--- Макеты/Log in page (client)/script.js
+++ Макеты/Log in page (client)/script.js
@@ -1,10 +1,8 @@
 function reg_form() {
 	document.getElementById('choice').style.display = 'none';
 	document.getElementById('reg-form').style.display = 'block';
-	document.getElementsByTagName('h1')[0].style.display = 'none';
 }
 function log_form() {
 	document.getElementById('choice').style.display = 'none';
 	document.getElementById('log-form').style.display = 'block';
-	document.getElementsByTagName('h1')[0].style.display = 'none';
 }
