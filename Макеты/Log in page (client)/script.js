5e4dde72492c8e9b1abe881dfe82528447c9ecac
IOMOIO-hub <69336443+IOMOIO-hub@users.noreply.github.com>
2020-12-13 14:08:35 +0500

    Omega animated log in page (client))
    uwu uwu uwu

--- Макеты/Log in page (client)/script.js
+++ Макеты/Log in page (client)/script.js
@@ -2,13 +2,13 @@ function reg_form() {
 	document.getElementById('choice').style.bottom = '-100%';
 	document.getElementById('reg-form').style.bottom = '0';
 	document.getElementsByTagName('body')[0].style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
-	document.getElementById('logo').style.filter = 'brightness(0.5)';
+	document.getElementById('logo').style.filter = 'brightness(0.6)';
 }
 function log_form() {
 	document.getElementById('choice').style.bottom = '-100%';
 	document.getElementById('log-form').style.bottom = '0';
 	document.getElementsByTagName('body')[0].style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
-	document.getElementById('logo').style.filter = 'brightness(0.5)';
+	document.getElementById('logo').style.filter = 'brightness(0.6)';
 }
 function close_form() {
 	document.getElementById('log-form').style.bottom = '-100%';
